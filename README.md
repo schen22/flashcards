63f9434c7c27c504a47aea98c7b3ceef9b26fd8a
schen22 <sarahchen22@gmail.com>
2020-10-23 21:27:08 -0700

    actual changes

---
+++ README.md
@@ -0,0 +1,14 @@
+# flashcards
+
+Playing around with SwiftUI while helping myself learn Japanese. 
+
+For now: super simple randomizer on list of hiragana and katakana I added myself
+
+Later on ideas to add:
+- Add all hiragana/katakana more smartly
+- Add audio for pronunciation
+- Add ability to self mark right/wrong
+- Store what was right/wrong to redo
+- Store count of right/wrong to retry 
+- stretch: drag and drop to match sounds/characters
+- stretch: enable shuffle mode vs cheat sheet to go through a e i o u order
--- flashcards/ContentView.swift
+++ flashcards/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  flashcards
+//  testing
 //
 //  Created by Sarah Chen on 10/23/20.
 //  Copyright © 2020 Sarah Chen. All rights reserved.
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+var vocab = ["あ","ア" ,"ザ" ,"ざ","エ","え","ビ","び"]
+var pronunications = ["a", "a", "za", "za", "e", "e", "bi", "bi"]
+let vocabDict = Dictionary(uniqueKeysWithValues: zip(vocab,pronunications))
+
 struct ContentView: View {
+    @State var shownWord : String = getRandomWord()
+    @State var showPronunciation : Bool = false
     var body: some View {
-        Text("Hello, World!")
+        // on tapping text, show value of key lulz.
+        Text("\(self.shownWord)").onTapGesture(perform: {
+            self.showPronunciation = !self.showPronunciation
+            if (self.showPronunciation) {
+                self.shownWord = vocabDict[self.shownWord] ?? ""
+            } else {
+                self.shownWord = getRandomWord()
+            }
+        })
+            .foregroundColor(Color.white)
+            .font(.system(size: 80, weight: .heavy, design: .default))
+            .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.width, minHeight: 0, maxHeight: UIScreen.main.bounds.height, alignment: .center)
+            .background(Color.black)
     }
 }
 
@@ -18,4 +36,8 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func getRandomWord() -> String {
+    return vocab[vocab.index(vocab.startIndex, offsetBy: Int.random(in:0..<vocab.count))]
 }
