60d813b8ec415bba8dfe27077a773895c7c76ee6
schen22 <sarahchen22@gmail.com>
2020-10-23 20:45:45 -0700

    Initial Commit

---
+++ flashcards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  flashcards
+//
+//  Created by Sarah Chen on 10/23/20.
+//  Copyright © 2020 Sarah Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
